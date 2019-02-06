afd84624cbdb9e2215f8991c88e1f02b74328d21
Timothy Ngo <timothyngo08@gmail.com>
2019-02-05 20:22:13 -0800

    improve hash searching

--- src/launcher.c
+++ src/launcher.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 20:47:39 by marvin            #+#    #+#             */
-/*   Updated: 2019/02/03 02:58:13 by tingo            ###   ########.fr       */
+/*   Updated: 2019/02/05 18:27:40 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int				ft_execvp(const char *path, char *const argv[])
+int				ft_execvp(const char *filename, char *const argv[])
 {
-	if (ft_strchr(path, '/') != NULL)
-
+	if (ft_strchr(filename, '/') != NULL)
+	{
+		execve(filename, argv, __environ);
+		return (-1);
+	}
 }
 
 int 			ms_launcher(char **args)
@@ -49,9 +52,11 @@ int 			ms_exec(char **args)
 	unsigned long hash;
 
 	hash = sbdm((unsigned char *)args[0]) / MAP_SIZE;
-	if (g_map[hash].name != NULL)
-		return(g_map[hash].f(args));
-	else
-		return(ms_launcher(args));
-	return (1);
+	while (hash < MAP_SIZE && g_map[hash].name != NULL)
+	{
+		if (ft_strcmmp(g_map[hash].name, args[0]) == 0)
+			return(g_map[hash].f(args));
+		hash++;
+	}
+	return(ms_launcher(args));
 }
