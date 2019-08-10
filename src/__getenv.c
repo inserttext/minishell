5c18fe0a4692b25c96c8ace5a4abfd2534c3615a
Timothy Ngo <timothyngo08@gmail.com>
2019-08-10 01:17:31 -0700

    remove unnecessary = check

--- src/__getenv.c
+++ src/__getenv.c
@@ -5,11 +5,9 @@ char	*__getenv(char *name)
 	size_t	len;
 	int		i;
 
-	i = 0;
 	if (name == NULL || (len = ft_strlen(name)) == 0)
 		return (NULL);
-	if (name[len - 1] == '=')
-		len -= 1;
+	i = 0;
 	while (__environ[i] != NULL)
 	{
 		if (__environ[i][len] == '=' &&
