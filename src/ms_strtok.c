6ce88e55744a612f88e0d14d98bc9af29d56675b
Timothy Ngo <timothyngo08@gmail.com>
2019-12-20 10:00:52 -0800

    Continue work on tokenization

--- src/ms_strtok.c
+++ src/ms_strtok.c
@@ -13,23 +13,48 @@
 #include "minishell.h"
 #include "libft.h"
 
-char		*brack(char *in, char d, char **save, ssize_t *len)
+char		*brack(char *s, char del, char **save, ssize_t *len)
+{
+	char *end;
+	char d[2];
+
+	d[0] = del;
+	d[1] = '\0';
+	end = s;
+	while (1)
+	{
+		end += ft_strcspn(end, d);
+		if (*end == '\0')
+			write(1, "Extend line", 1);
+		else if (end[-1] == '\\')
+			continue;
+		else
+			break;
+	}
+	*end = '\0';
+	*save = end + 1;
+	return (s);
+}
+
+char		*brack2(char *in, char **save, ssize_t *len)
 {
 
 }
 
 char		*ms_strtok(char **s, ssize_t *len)
 {
-	static char	**olds;
-	char		*in;
+	static char	**hold;
+	static char	*olds;
 
 	if (*s != NULL)
-		olds = s;
-	in = *olds;
-	in += ft_strspn(in, " \t");
-	if (*in == '\0')
+	{
+		hold = s;
+		olds = *s;
+	}
+	olds += ft_strspn(olds, " ");
+	if (*olds == '\0')
 		return (NULL);
-	if (*in == '"' || *in == '\'')
-		return (brack(in + 1, *in, olds, len));
-	return (brack(in + 1, *in, olds, len));
+	if (*olds == '"' || *olds == '\'')
+		return (brack(olds + 1, *olds, &olds, len));
+	return (brack2(olds, &olds, len));
 }
